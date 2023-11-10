36d191c55f1631f1a2e9ef662e9b3e99bbac6d2b
mezianiimad <meziani.imad98@gmail.com>
2023-11-10 02:05:36 +0100

    ADD V1 of AK

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,18 +1,24 @@
 import { Title } from "solid-start";
-import Counter from "~/components/Counter";
+
 
 export default function Home() {
   return (
     <main>
-      <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
+      <Title>EURL CODIMOS</Title>
+      <h1>EURL CODIMOS</h1>
+      <h3>COMPTOIR DE DISTRIBUTION DU MATÉRIEL ET OUTILLAGE SONELGAZ</h3>
+      <img src="public\codimos logo.png" alt="Codimos LOGO">
       <p>
-        Visit{" "}
-        <a href="https://main--startling-daifuku-bb8624.netlify.app/YacineTV__V3.apk" target="_blank">
-          Download codimos
-        </a>{" "}
-        to learn how to build SolidStart apps.
+        Cliquez sur le lien pour télécharger l'APK de l'application utilisateur {" "}
+        <a href="https://main--startling-daifuku-bb8624.netlify.app/CodimosApp.apk" target="_blank">
+          CodimosApp
+        </a>{
+      </p>
+        <p>
+          Cliquez sur le lien pour télécharger l'APK de l'application administrateur {" "}
+          <a href="https://main--startling-daifuku-bb8624.netlify.app/CodimosAppAdmin.apk" target="_blank">
+            CodimosAppAdmin
+          </a>{
       </p>
     </main>
   );
