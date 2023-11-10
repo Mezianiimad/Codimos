3b7ed0fbd380023a8d5e5e6a6a1b6337d4e6d2e6
mezianiimad <meziani.imad98@gmail.com>
2023-11-10 02:53:09 +0100

    DEV

--- cypress/e2e/basic.cy.ts
+++ cypress/e2e/basic.cy.ts
@@ -5,13 +5,13 @@ describe('empty spec', () => {
 
   it('display header text', () => {
     cy.get('h1')
-      .contains('Hello World!', { matchCase: false })
+      .contains('EURL CODIMOS!', { matchCase: false })
       .contains('', { matchCase: false })
   })
 
-  it('displays Click button', () => {
-    cy.get('button')
-      .contains('Clicks');
-    .contains('');
-  })
+  // it('displays Click button', () => {
+  //   cy.get('button')
+  //     .contains('Clicks');
+  //   .contains('');
+  // })
 })
