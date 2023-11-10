526acf16555bab01d3fd4f725c60e795437877f8
mezianiimad <meziani.imad98@gmail.com>
2023-11-10 02:51:18 +0100

    DEV

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,8 +5,9 @@ export default function Home() {
   return (
     <main>
       <Title>Hello World</Title>
-      <h1>Hello world!</h1>
-      <Counter />
+      <h1>EURL CODIMOS</h1>
+      <h3>COMPTOIR DE DISTRIBUTION DU MATÉRIEL ET OUTILLAGE SONELGAZ</h3>
+      <img src="/public/codimos_logo.png" alt="codimos logo" width="450" height="200">
       <p>
         Visit{" "}
         <a href="https://main--startling-daifuku-bb8624.netlify.app/CodimosApp.apk" target="_blank">
