5a74ef1eb6d633216a808cfef3c1f67fa4fa49b4
mezianiimad <meziani.imad98@gmail.com>
2023-11-11 08:53:42 +0100

    Last version

--- cypress/e2e/basic.cy.ts
+++ cypress/e2e/basic.cy.ts
@@ -5,7 +5,7 @@ describe('empty spec', () => {
 
   it('display header text', () => {
     cy.get('h1')
-      .contains('EURL CODIMOS', { matchCase: false })
+    .contains('Hello World!', { matchCase: false })
     .contains('', { matchCase: false })
   })
 
